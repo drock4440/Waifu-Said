a0f09635b8be1919cfe5ca00911bb71ffd27de09
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-06-02 20:25:22 -0700

    add buttons, favicon, search by

--- ghibli-said/script.js
+++ ghibli-said/script.js
@@ -1,3 +1,7 @@
 fetch('https://ghibliapi.herokuapp.com/films/')
   .then(response => response.json())
   .then(data => console.log(data));
+
+fetch('https://api.waifu.pics/sfw/neko')
+  .then(response => response.json())
+  .then(quote => console.log(quote))
