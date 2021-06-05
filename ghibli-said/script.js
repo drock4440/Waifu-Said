6ff4ac5da387825077bea90015d11661e778bc8b
Prometheus <atommielcarek@gmail.com>
2021-06-05 11:59:48 -0600

    api calls added for categories

--- ghibli-said/script.js
+++ ghibli-said/script.js
@@ -1,7 +1,40 @@
-fetch('https://ghibliapi.herokuapp.com/films/')
-  .then(response => response.json())
-  .then(data => console.log(data));
 
 fetch('https://api.waifu.pics/sfw/neko')
   .then(response => response.json())
   .then(quote => console.log(quote))
+ 
+ fetch('https://api.waifu.pics/sfw/shinobu')
+  .then(response => response.json())
+  .then(quote => console.log(quote))
+
+fetch('https://api.waifu.pics/sfw/lick')
+  .then(response => response.json())
+  .then(quote => console.log(quote))
+
+fetch('https://api.waifu.pics/sfw/kill')
+  .then(response => response.json())
+  .then(quote => console.log(quote))  
+
+fetch('https://api.waifu.pics/sfw/nom')
+  .then(response => response.json())
+  .then(quote => console.log(quote))
+
+fetch('https://api.waifu.pics/sfw/smug')
+  .then(response => response.json())
+  .then(quote => console.log(quote))  
+
+fetch('https://api.waifu.pics/sfw/wink')
+  .then(response => response.json())
+  .then(quote => console.log(quote))
+  
+fetch('https://api.waifu.pics/sfw/waifu')
+  .then(response => response.json())
+  .then(quote => console.log(quote))  
+
+fetch('https://api.waifu.pics/sfw/cry')
+  .then(response => response.json())
+  .then(quote => console.log(quote))  
+
+fetch('https://api.waifu.pics/sfw/hug')
+  .then(response => response.json())
+  .then(quote => console.log(quote))cd 
