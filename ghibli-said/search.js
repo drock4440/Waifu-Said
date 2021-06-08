65ca2104a0e2df00b1e641a53c070e63c224745e
Drock4440 <daltonrothrock8@gmail.com>
2021-06-08 12:37:06 -0600

    adding quote by character

--- ghibli-said/search.js
+++ ghibli-said/search.js
@@ -1,17 +1,36 @@
 var searchBar = document.getElementById('searchBar')
 const animeCharacters = []
 var searchBtn = document.getElementById('search-button')
-var input = document.getElementById('search-input')
-var charName = input.value
 
+searchBtn.addEventListener('click', function(getByChar) {
+    getByChar.preventDefault()
+  var input = document.getElementById('search-input');
+  var charName = input.value;
+            console.log(charName)
+            
+            let queryUrl = "https://animechan.vercel.app/api/quotes/character?name=" + charName
+            try {
+             fetch(queryUrl)
+            .then(response => response.json())
+            .then(quotes => {
+              const quoteDiv = document.getElementById("quotes")
+              const quote = quotes[0];
+               if(!quote) {
+                const quoteDiv = document.getElementById("quotes")
+                return quoteDiv.innerHTML += `<li class="failed"> couldnt find a quote from ${charName} </li>`
+               }
+            quoteDiv.innerHTML = ""
+              console.log(quote)
+              return quoteDiv.innerHTML +=`<ul>
+              <li class="character">${quote.character}</li>
+              <li class="anime">${quote.anime}</li>
+              <li>quote:</li>
+              <q class="quote">${quote.quote}</q>
+              </ul>`
+            })
+            } catch(err) {
+              const quoteDiv = document.getElementById("quotes")
+              quoteDiv.innerHTML +=`<li class="failed"> couldnt find a quote from ${charnName} </li>`
+            }
+    })
 
-function getByChar (charName) {
-            let queryUrl = "https://animechan.vercel.app/api/quotes/character?name=" + charName 
-            fetch(queryUrl)
-            .then (function(response){
-            console.log(response);
-      })
-    
-    }
-
-searchBtn.addEventListener('click', getByChar)
